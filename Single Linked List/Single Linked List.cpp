c1aecbbd770075ac7231c77bd2ee9379987554fc
finanovita <finanovita479@gmail.com>
2024-04-25 12:37:28 +0700

    Membandingkan data untuk membaca data

--- Single Linked List/Single Linked List.cpp
+++ Single Linked List/Single Linked List.cpp
@@ -63,6 +63,19 @@ bool serachNode(int nim, Node* current, Node* previous) {
 		current = current->next;
 	}
 
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
 }
 
 
